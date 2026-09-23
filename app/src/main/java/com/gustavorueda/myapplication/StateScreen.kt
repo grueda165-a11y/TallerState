5cfbe4fe122096353904ef4524b77ee8396821ca
Facultad De Sistemas <sistemas@academico.unab.ad>
2026-09-23 15:37:51 -0500

    contador funcional

--- app/src/main/java/com/gustavorueda/myapplication/StateScreen.kt
+++ app/src/main/java/com/gustavorueda/myapplication/StateScreen.kt
@@ -1,31 +1,45 @@
 package com.gustavorueda.myapplication
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.log
 
 
 @Preview(showSystemUi = true)
 @Composable
 fun StateScreen(){
+
+    //var contador
+    var contador by remember { mutableStateOf( 3) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("contador: 0", fontSize = 32.sp)
+        Text("contador: $contador", fontSize = 32.sp)
         Spacer(modifier = Modifier.height(25.dp))
-        Button(onClick = {}) {
+        Button(onClick = {
+            Log.i("mis-errores",  "estoy dando click")
+            contador = contador + 1
+        }) {
             Text("sumar 1")
         }
     }
