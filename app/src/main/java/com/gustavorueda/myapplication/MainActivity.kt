854d05490bae32b2963986b15b92929854589260
Facultad De Sistemas <sistemas@academico.unab.ad>
2026-09-24 17:30:08 -0500

    Dado Funcional

--- app/src/main/java/com/gustavorueda/myapplication/MainActivity.kt
+++ app/src/main/java/com/gustavorueda/myapplication/MainActivity.kt
@@ -1,26 +1,66 @@
 package com.gustavorueda.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.gustavorueda.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            StateScreen()
-
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    DiceScreen()
+                }
+            }
         }
     }
 }
 
+@Preview(showSystemUi = true)
+@Composable
+fun DiceScreen() {
+    val dados = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+
+    var dadoActual by remember { mutableStateOf(dados[0]) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = dadoActual),
+            contentDescription = "Dado",
+            modifier = Modifier.size(200.dp)
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Button(onClick = {
+            Log.i("mis errores", "Estoy dando click")
+            val numero = (1..6).random()
+            dadoActual = dados[numero - 1]
+        }) {
+            Text("Lanza el dado!!!!")
+        }
+    }
+}
